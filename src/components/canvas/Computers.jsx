ff27465cb53feb213eec0a7f457e6fc1d59c81c3
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2023-03-16 22:10:52 +0000

    fixed computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,18 +9,17 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} 
-      groundColor="black" />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
       <primitive 
         object={computer.scene} 
         scale={0.75}
         position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        rotation={[-0.01, 0.05, -0.1]}
       />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
   return (
@@ -28,20 +27,19 @@ const ComputersCanvas = () => {
       frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls 
-        enableZoom={false}
-        maxPolarAngle={Math.PI /2}
-        minPolarAngle={Math.PI /2} 
-      />
-      <Computers />
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
       </Suspense>
 
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
-export default Computers
+export default ComputersCanvas;
